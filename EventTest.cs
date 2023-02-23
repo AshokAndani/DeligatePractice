4f35a5a2d8501c835f8e88fc2d1154326c4279f5
ashokk <ashokk@nextgen.com>
2023-02-23 06:29:04 +0530

    initial commit

---
+++ EventTest.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace SampleApp
+//{
+//    public delegate void MyDeligate(string str);
+
+//    class Program
+//    {
+//        event MyDeligate MyEvent;
+
+//        public Program()
+//        {
+//            this.MyEvent += new MyDeligate(this.WriteToConsole);
+//        }
+//        public void WriteToConsole(string output)
+//        {
+//            Console.WriteLine(output);
+//        }
+//        static void Main(string[] args)
+//        {
+//            Program p = new Program();
+//            // its a console so we cannot have a button
+//            // explicitly calling the event 
+//            p.MyEvent("triggered after click event....!");
+//        }
+//    }
+//}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Deligates
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    
+    internal class Program
+    {
+        public delegate bool FilterDelegate(Person p);
+        
+
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            var pp = IsChild(p);
+            var dd = new Program();
+            
+            Person p1 = new Person() { Name = "ratan", Age = 56 };
+            Person p2 = new Person() { Name = "kamlesh", Age = 70 };
+            Person p3 = new Person() { Name = "rajesh", Age = 25 };
+            Person p4 = new Person() { Name = "anup", Age = 17 };
+            List<Person> list = new List<Person>();
+            list.Add(p1);
+            list.Add(p2);
+            list.Add(p3);
+            list.Add(p4);
+
+
+            //del += IsSenior;
+            //del += IsAdult;
+
+            //del.Invoke(p4);
+
+            FilterDelegate del = new FilterDelegate(IsChild);
+            Console.WriteLine("Children:");
+            DisplayPeople(list, del);
+            Console.WriteLine("Adults");
+            DisplayPeople(list, new FilterDelegate(IsAdult));
+            Console.WriteLine("Senior:");
+            DisplayPeople(list, new FilterDelegate(IsSenior));
+
+
+
+        }
+        static bool IsChild(Person p)
+        {
+            Console.WriteLine("Executed isChild");
+            bool isChild= p.Age < 18;
+            return isChild;
+        }
+
+        static bool IsAdult(Person p)
+        {
+            //Console.WriteLine("Executed isAdult");
+            return p.Age >= 18 && p.Age < 70;
+        }
+
+        static bool IsSenior(Person p)
+        {
+            Console.WriteLine("Executed isSenior");
+            return p.Age >= 65;
+        }
+        static void DisplayPeople(List<Person> people, FilterDelegate filter)// isChild
+        {
+
+            foreach (Person p in people)
+            {
+              bool flag =filter(p) ;
+                if (flag)
+                {
+                    Console.WriteLine("{0}, {1} years old", p.Name, p.Age);
+                }
+            }
+            Console.Write("\n\n");
+        }
+
+    }
+}
